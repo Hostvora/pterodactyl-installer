62a7528054337ad6c939fe73c05b4b457c23bb7c
Fambos <ytfambos@gmail.com>
2026-06-29 15:42:03 +0200

    Update GITHUB_BASE_URL to new repository

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ set -e
 
 export GITHUB_SOURCE="v1.3.0"
 export SCRIPT_RELEASE="v1.3.0"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/hostvora/pterodactyl-installer"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
